112e939447e3c510f9e7d4fc30f76e039f1ba186
Jonathan Mason <jonathan.mason.app@gmail.com>
2021-12-04 00:12:43 +0000

    Backup of work in progress.

---
+++ SuperheroSquad/Model/Marvel/MarvelResultCharacter.swift
@@ -0,0 +1,15 @@
+//
+//  MarvelResultResponseCharacter.swift
+//  SuperheroSquad
+//
+//  Created by Jonathan Mason on 03/12/2021.
+//
+
+import Foundation
+
+/// Part of JSON response object for Marvel API results.
+struct MarvelResultCharacter: Codable {
+    let name: String
+    let description: String
+    let thumbnail: Data
+}

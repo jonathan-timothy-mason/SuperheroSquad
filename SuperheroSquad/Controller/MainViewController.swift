08d5b2a16bc7886c9413e32f56fc5eed8fd45f3c
Jonathan Mason <jonathan.mason.app@gmail.com>
2021-12-21 22:27:24 +0000

    Improved sizing of thumbnails in CollectionView.

--- SuperheroSquad/Controller/MainViewController.swift
+++ SuperheroSquad/Controller/MainViewController.swift
@@ -21,15 +21,6 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // From "Code for Collection View Flow Layout" of "Lesson 8: Complete
-        // the MemeMe App".
-        // Size cells according to screen size.
-        let space: CGFloat = 3.0
-        let dimension = (view.frame.size.width - (2 * space)) / 3.0
-        flowLayout.minimumInteritemSpacing = space
-        flowLayout.minimumLineSpacing = space
-        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
-
         // Load squad from data store.
         loadSquad()
         

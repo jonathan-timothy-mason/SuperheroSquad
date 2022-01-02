a6bef1e7f5603d935789c100ea0fec8c204b5948
Jonathan Mason <jonathan.mason.app@gmail.com>
2022-01-02 18:50:25 +0000

    Finished implementing paging.

--- SuperheroSquad/Controller/MainViewController.swift
+++ SuperheroSquad/Controller/MainViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    
+    /// Character pages, one for each letter of alphabet.
     enum Pages: Int {
         case A = 0
         case B = 1
@@ -37,20 +37,24 @@ class MainViewController: UIViewController {
         case Y = 24
         case Z = 25
     }
-    
-    var squad: [SquadMemberProtocol] = []
-    
+       
+    /// Cache of characters for each page.
     var characters: [Pages: [Character]] = [Pages.A:[], Pages.B:[], Pages.C:[], Pages.D:[], Pages.E:[], Pages.F:[], Pages.G:[], Pages.H:[], Pages.I:[], Pages.J:[], Pages.K:[], Pages.L:[], Pages.M:[], Pages.N:[], Pages.O:[], Pages.P:[], Pages.Q:[], Pages.R:[], Pages.S:[], Pages.T:[], Pages.U:[], Pages.V:[], Pages.W:[], Pages.X:[], Pages.Y:[], Pages.Z:[]]
     var currentPage = Pages.A
-
+    var squad: [SquadMemberProtocol] = []
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     @IBOutlet weak var pager: UIPageControl!
+    @IBOutlet weak var currentPageLabel: UILabel!
        
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Prevent rapid changing of page generating lots of requests.
+        pager.allowsContinuousInteraction = false
+        
         // Load squad from data store.
         loadSquad()
         
@@ -64,11 +68,22 @@ class MainViewController: UIViewController {
         tableView.reloadData()
     }
     
-    /// Load characters from Marvel API for current page.
+    /// Load characters for current page from cache or Marvel API.
     /// - Note: Getting enum name from answer to "How to get the name of enumeration value in Swift?" by Stuart:
     /// https://stackoverflow.com/questions/24113126/how-to-get-the-name-of-enumeration-value-in-swift
     func loadCharactersForCurrentPage() {
-        MarvelClient.getCharacters(letter: "\(currentPage)", numberDownloaded: 0, completion: handleResponseToLoadCharacters)
+        if characters[currentPage]!.count > 0 {
+            // Reload cached page of characters.
+            collectionView.reloadData()
+            collectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
+        }
+        else {
+            pager.isEnabled = false // Pevent page change whilst getting characters.
+            
+            // Load for first time from Marvel API.
+            collectionView.reloadData() // Clear immediately to indicate something is happening.
+            MarvelClient.getCharacters(letter: "\(currentPage)", numberDownloaded: 0, completion: handleResponseToLoadCharacters)
+        }
     }
     
     /// Handle response to load characters.
@@ -77,6 +92,8 @@ class MainViewController: UIViewController {
     ///   - totalNumberCharacters: Total number of characters.
     ///   - error: Error, if there was a problem.
     func handleResponseToLoadCharacters(newCharacters: [Character], totalNumberCharacters: Int, error: Error?) {
+        pager.isEnabled = true // Allow page change now request has finished.
+        
         guard error == nil else {
             ControllerHelpers.showMessage(parent: self, caption: "Marvel API Error", introMessage: "There was a problem downloading characters using the Marvel API.", error: error)
             return
@@ -85,6 +102,22 @@ class MainViewController: UIViewController {
         // Update placeholders with newly loaded characters.
         characters[currentPage] = newCharacters
         self.collectionView.reloadData()
+        collectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
+    }
+    
+    /// Handle change of page and display corresponding caharacters.
+    @IBAction func pagerChanged() {
+        let newPage = Pages(rawValue: pager.currentPage)!
+        if newPage != currentPage {
+            // Change current page.
+            currentPage = newPage
+            
+            // Update current page label.
+            currentPageLabel.text = "\(currentPage)"
+            
+            // Display characters corresponding to page.
+            loadCharactersForCurrentPage()
+        }
     }
 }
 
